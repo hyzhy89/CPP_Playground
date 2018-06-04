bf6063ae6da481305b5bb37e6c03c3deb3085f34
hyzhy89 <hyzhy891@gmail.com>
2018-06-04 20:47:15 +0800

    Test Commit;

---
+++ Playground_CPP/Playground_CPP/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Playground_CPP
+//
+//  Created by Haoyu Zhang on 2018/6/4.
+//  Copyright © 2018 Haoyu Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Test Git Commit!" << std::endl;
+    std::cout << "Hello, World!\n";
+    return 0;
+}
